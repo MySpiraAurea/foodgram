85ecf0273b8573a215948bae4509b4203752cc53
Ilya Arkhipov <arkhipovilya1605@gmail.com>
2025-08-16 23:42:46 +0300

    Настроить git actions

--- backend/api/serializers/recipe_serializers.py
+++ backend/api/serializers/recipe_serializers.py
@@ -1,6 +1,5 @@
 # api/serializers/recipe_serializers.py
 from rest_framework import serializers
-from rest_framework.validators import UniqueTogetherValidator
 
 from recipes.models import (
     Ingredient,
